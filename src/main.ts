b6d6b59dc4dc9a735d00a236007ed9140c5bc0f7
youncccat <1306036576@qq.com>
2020-02-05 12:16:28 +0800

    [Feat] add description for command

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,15 @@ import { initHandler, bootstrapHandler, updateHandler } from "./handlers";
 Bricor.version("1.0.0");
 
 // commands
-Bricor.command("init").action(initHandler);
-Bricor.command("bootstrap").action(bootstrapHandler);
-Bricor.command("update <repo> <branch>").action(updateHandler);
+Bricor.command("init")
+  .description("quickly create a config file")
+  .action(initHandler);
+Bricor.command("bootstrap")
+  .description(`pull all the repositories that defined in the ".bricor.json"`)
+  .action(bootstrapHandler);
+Bricor.command("update <repo> <branch>")
+  .description("update submodules from specified branch")
+  .action(updateHandler);
 
 // bootstrap! read all args into commander for parse
 Bricor.parse(process.argv);
